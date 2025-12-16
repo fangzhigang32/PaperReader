f67b99dc97b18ee4cc101112bb717e7ecc176abe
AlphaGogoo <1411420236@qq.com>
2025-12-16 18:14:16 +0800

    '增加自定义研究方向与领域'

--- src/getPaper.py
+++ src/getPaper.py
@@ -153,8 +153,7 @@ def get_arxiv_records(searchText, createdTime):
 
     url = "http://export.arxiv.org/api/query?"
     params = {
-        # "search_query": f"all:{searchText} AND lastUpdatedDate:[{createdTime.replace("-", "")}0000 TO {createdTime.replace("-", "")}2359]",
-        "search_query": f"lastUpdatedDate:[{createdTime.replace("-", "")}0000 TO {createdTime.replace("-", "")}2359]",
+        "search_query": f"all:{searchText} AND lastUpdatedDate:[{createdTime.replace('-', '')}0000 TO {createdTime.replace('-', '')}2359]",
         "sortBy": "relevance",
         "sortOrder": "descending",
         "max_results": arXivCount,
@@ -200,7 +199,7 @@ def get_crossref_ieee_acm_records(searchText, createdTime):
 
     url = "https://api.crossref.org/works"
     params = {
-        # "query": searchText,
+        "query": searchText,
         "sort": "relevance",
         "order": "desc",
         "rows": crossrefCount,
@@ -296,8 +295,3 @@ def getAllPapers(searchText, createdTime):
 # 脚本入口
 # =============================
 # getAllPapers(searchText)
-
-
-
-
-
